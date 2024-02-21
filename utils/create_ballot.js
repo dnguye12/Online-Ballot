662cc03bae5720a2d62e844eada6138d9ac85572
Duc Huy Nguyen <duc-huy.nguyen@universite-paris-saclay.fr>
2024-02-21 08:51:16 +0100

    remove questions, choices, emails when creating a ballot

--- utils/create_ballot.js
+++ utils/create_ballot.js
@@ -14,12 +14,13 @@ function addQuestion() {
     const questionDiv = document.createElement('div');
     questionDiv.id = 'question' + questionCount;
     questionDiv.innerHTML = `
-        <label for="question${questionCount}Title">Question ${questionCount}:</label>
+        <label for="question${questionCount}Title">Question:</label>
         <input type="text" id="question${questionCount}Title" name="question${questionCount}Title" required><br>
         <div id="choicesContainer${questionCount}">
             <!-- Choices for question ${questionCount} will be added here -->
         </div>
         <button type="button" onclick="addChoice(${questionCount})">Add Choice</button><br>
+        <button type="button" onclick="deleteElement('question${questionCount}')">Remove Question</button>
     `;
     questionsContainer.appendChild(questionDiv);
     addChoice(questionCount); // Automatically add one choice field
@@ -28,11 +29,13 @@ function addQuestion() {
 function addChoice(questionNumber) {
     const choicesContainer = document.getElementById('choicesContainer' + questionNumber);
     const choiceCount = choicesContainer.childElementCount + 1;
-    const choiceInput = document.createElement('input');
-    choiceInput.type = 'text';
-    choiceInput.id = `question${questionNumber}Choice${choiceCount}`;
-    choiceInput.name = `question${questionNumber}Choice${choiceCount}`;
-    choicesContainer.appendChild(choiceInput);
+    const choiceDiv = document.createElement('div');
+    choiceDiv.id = `question${questionNumber}Choice${choiceCount}Div`;
+    choiceDiv.innerHTML = `
+        <input type="text" id="question${questionNumber}Choice${choiceCount}" name="question${questionNumber}Choice${choiceCount}" required>
+        <button type="button" onclick="deleteElement('${choiceDiv.id}')">Delete Choice</button>
+    `;
+    choicesContainer.appendChild(choiceDiv);
 }
 
 let voterCount = 0;
@@ -44,6 +47,14 @@ function addVoter() {
     voterDiv.innerHTML = `
     <label for="voter${voterCount}">Email ${voterCount}:</label>
     <input type="text" id="voter${voterCount}" name="voter${voterCount}" required><br>
+    <button type="button" onclick="deleteElement('voter${voterCount}')">Remove Voter Email</button>
     `;
     votersContainer.appendChild(voterDiv);
+}
+
+function deleteElement(id) {
+    const element = document.getElementById(id);
+    if(element) {
+        element.parentNode.removeChild(element);
+    }
 }
